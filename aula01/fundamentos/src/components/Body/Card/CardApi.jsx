50d6f443a8218675fdba7e3739b8f0df5568d431
Eric Mara <ericmaraoliveira@gmail.com>
2025-08-21 22:11:03 -0300

    função de filtro, e aplicação de react router

--- aula01/fundamentos/src/components/Body/Card/CardApi.jsx
+++ aula01/fundamentos/src/components/Body/Card/CardApi.jsx
@@ -12,22 +12,28 @@ export const CardApi = () => {
     //     setContador(prev => prev + 1)
     // }
 
-    const [users, setUsers] = useState([])
+    const [users, setUsers] = useState([]);
+    const [filtro, setFiltro] = useState("");
 
     useEffect(() => {
         fetch('https://jsonplaceholder.typicode.com/users')
             .then(res => res.json())
             .then(data => {
-                setUsers(data)
+                const filtrados = data.filter((user) => user.name.toLowerCase().includes(filtro.toLowerCase()))
+                setUsers(filtrados)
             })
         console.log(users)
-    }, [])
+    }, [filtro])
 
     return (
-        <>
+        <div className={styles.cardContainerApi}>
             {/* <p>{contador}</p>
             <button onClick={incrementaValor}>Incrementa</button> */}
-            <div className={styles.cardContainerApi}>
+            <div>
+                <input type="text" className={styles.inputFiltro} placeholder="Filtrar por nome..." value={filtro} onChange={(e) => setFiltro(e.target.value)} />
+            </div>
+
+            <div className={styles.cardApi}>
                 {users.map((user) => (
                     <div className={styles.card} key={user.id}>
                         <h3>{user.name}</h3>
@@ -37,7 +43,7 @@ export const CardApi = () => {
                 ))
                 }
             </div>
-        </>
+        </div>
     )
 }
 
