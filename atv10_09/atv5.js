08cc3a1fe5e3cbcdf7bb119c0b92bf0bd984afe5
Eric Mara <ericmaraoliveira@gmail.com>
2025-09-10 22:05:06 -0300

    continução atividade

--- atv10_09/atv5.js
+++ atv10_09/atv5.js
@@ -8,20 +8,25 @@ function linearSearch(arr, target) {
 function binarySearch(arr, target) {
     if(target == null || target === undefined) return
     const numerosOrdenados = arr.sort((a, b) => a - b);
-    const 
+    const metadeArray = Math.floor(numerosOrdenados.length / 2)
 
-    for (let i = 0; i <= arr.length; i++) {
-        if(numerosOrdenados.length / 2 < target){
-
+    for (let i = 0; i <= numerosOrdenados.length; i++) {
+        if( numerosOrdenados[metadeArray] < target){
+            for(let i = 0; i <= metadeArray; i++){
+                if (numerosOrdenados[i] === target) return numerosOrdenados.indexOf(numerosOrdenados[i]);
+            }
+        }else if(numerosOrdenados[metadeArray] > target){
+            for(let i = metadeArray; i <= metadeArray; i++){
+                if (numerosOrdenados[i] === target) return numerosOrdenados.indexOf(numerosOrdenados[i]);
+            }
         }
-        if (numerosOrdenados[i] === target) return numerosOrdenados.indexOf(numerosOrdenados[i]);
     }
 
 }
 
 function buscaValores() {
-    console.log(linearSearch([3, 1, 4], 3));
-    console.log(binarySearch([1, 3, 4, 10], 4));
+    console.log(linearSearch([3, 1, 4], 4));
+    console.log(binarySearch([1, 3, 4, 10], 10));
 }
 
 console.log(buscaValores())
---
+++ atv10_09/atv6.js
@@ -0,0 +1,6 @@
+function enqueue2(value) { }
+function dequeue2() { }
+
+function juncaoFilas() {
+    
+}
