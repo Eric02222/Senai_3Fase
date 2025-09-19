d275dfc02c035d84c5e9d622bf3dab2642289023
Eric Mara <ericmaraoliveira@gmail.com>
2025-09-18 22:13:07 -0300

    context e tela de login

--- aula01/multi_page/src/layout/Header/Header.jsx
+++ aula01/multi_page/src/layout/Header/Header.jsx
@@ -1,6 +1,15 @@
-import { NavLink } from "react-router";
+import { NavLink, useNavigate } from "react-router";
+import { useAuth } from "../../context/AuthContext";
 
 const Header = () => {
+    const { user, logout } = useAuth();
+    const navigate = useNavigate();
+
+    const handleLogout = () => {
+        logout();
+        navigate("/")
+    }
+
     return (
         <header className='flex items-center justify-between p-4 bg-gray-200'>
             <nav className='flex gap-5'>
@@ -19,8 +28,20 @@ const Header = () => {
                 }>
                     Blog
                 </NavLink>
-            
+
             </nav>
+
+
+            <div>
+                {
+                    user ? (
+                        <>
+                            <span className="mr-4">Ola, {user.email}</span>
+                            <button onClick={handleLogout} className="bg-red-500 text-white px-3 py-1 rounded">Logout</button>
+                        </>
+                    ) : (<NavLink to="/login" className="bg-blue-600 text-white px-3 py-1 rounded">Login</NavLink>)
+                }
+            </div>
         </header>
     )
 }
--- aula01/multi_page/src/main.jsx
+++ aula01/multi_page/src/main.jsx
@@ -10,18 +10,24 @@ import Sobre from './pages/Sobre/Sobre';
 import { Main } from './layout/main/Main';
 import Blog from './pages/Blog/Blog';
 import PostDetail from './pages/Blog/PostDetail';
+import { AuthProvider } from './context/AuthContext';
+import Login from './pages/Login/Login';
 // import App from './App.jsx'
 
 const router = createBrowserRouter([
-{
-  element: <Main/>,
-  children: [
-    {path: "/", element: <Home />},
-    {path: "/sobre", element: <Sobre />},
-    {path: "/blog", element: <Blog />},
-    {path: "/post/:id", element: <PostDetail />},
-  ]
-}
+  {
+    element: <Main />,
+    children: [
+      { path: "/", element: <Home /> },
+      { path: "/sobre", element: <Sobre /> },
+      { path: "/blog", element: <Blog /> },
+      { path: "/post/:id", element: <PostDetail /> },
+    ]
+  },
+  {
+    path:"login",
+    element: <Login/>
+  }
 
   // {
   //   path: "/",
@@ -36,8 +42,10 @@ const router = createBrowserRouter([
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    {/* <App /> */}
+    <AuthProvider>
+      <RouterProvider router={router} />,
+    </AuthProvider>
 
-    <RouterProvider router={router} />,
+    {/* <App /> */}
   </StrictMode>,
 )
