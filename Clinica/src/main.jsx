2823bb8f4d516f20cdc14e467f2841e988947053
Eric Mara <ericmaraoliveira@gmail.com>
2025-09-25 22:06:54 -0300

    inicio plataforma clinica

---
+++ Clinica/src/main.jsx
@@ -0,0 +1,24 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+// import App from './App.jsx'
+
+//import react router
+import { createBrowserRouter } from 'react-router'
+import { RouterProvider } from 'react-router/dom'
+import Login from './pages/Login/Login'
+
+const router = createBrowserRouter([
+  {
+    path:"/",
+    element: <Login/>
+  },
+])
+
+createRoot(document.getElementById('root')).render(
+
+  <StrictMode>
+    <RouterProvider router={router}/>
+    {/* <App /> */}
+  </StrictMode>,
+)
